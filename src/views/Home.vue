250c063c9e5ba0957f87ab8ff9ad5fafa88f7b35
wiwth <3214533173@qq.com>
2021-12-21 20:44:07 +0800

    new project

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="no-padding">
+    <header class="home_header "></header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+  .home_header {
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+    height: 48px;
+    background: pink;
+  }
+</style>
